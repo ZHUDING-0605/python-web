b49a0e4bf90f53a6f052b10e87d3a13e910f0273
ZHUDING-0605 <2134231527@qq.com>
2024-12-26 15:15:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ if url:
 
         # 词云绘制图片
         st.subheader("词云图")
-        heart_mask_path = 'uploaded_image.png'
+        heart_mask_path = 'love.png'
 '
         heart_mask = np.array(Image.open(heart_mask_path))
 
