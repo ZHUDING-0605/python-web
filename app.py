2f7b41d9f1b395a0218fb3452177bba785098908
ZHUDING-0605 <2134231527@qq.com>
2024-12-30 15:14:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,10 @@ def clean_text(text):
 
 if url:
     try:
-        response = requests.get(url)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        }
+        response = requests.get(url,headers=headers)
         soup = BeautifulSoup(response.text, "html.parser")
         paragraphs = soup.find_all('body')
         text = " ".join([para.get_text() for para in paragraphs])
